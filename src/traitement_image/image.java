dab3ecccceae8eb17b8ef2b1c91e26f4b827f2a7
Maxime TERRADE <maxime.terrade@eleves.ec-nantes.fr>
2013-09-27 17:44:30 +0200

    debut codage

--- src/traitement_image/image.java
+++ src/traitement_image/image.java
@@ -1,5 +1,47 @@
 package traitement_image;
 
-public class image {
+import java.io.*;
 
+public class Image {
+	
+	int[][] pixels;
+	int x;
+	int y;
+	
+	public Image(String fichier) {
+		
+		int i=1;
+		//lecture du fichier texte	
+		try{
+			InputStream ips=new FileInputStream(fichier); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne;
+			while ((ligne=br.readLine())!=null) {
+				switch (i) {
+					case 3:
+						int[] tokens = ligne.split(" ");
+						this.x = tokens[0];
+						this.y = tokens[1];
+						break;
+	
+					default:
+						break;
+				}
+				
+			}
+			br.close(); 
+			System.out.println(x);
+			System.out.println(y);
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+	}
+	
+	public static void main(String[] args) {
+		Image i = new Image("C:\\Users\\max\\Desktop\\lena.pgm");
+		
+	}
+	
 }
